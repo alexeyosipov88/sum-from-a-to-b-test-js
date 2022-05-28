b6fa5313478ea1c00eba5bcac29ca8372768329c
alexeyosipov88 <aoosipov88@gmail.com>
2022-05-28 17:16:04 -0400

    add solution

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
-function sum(fromN, toN) {
+function  sum(fromN, toN) {
+  if(fromN == toN) {
+    return toN;
+  }
+  return fromN + sum(fromN + 1, toN);
   // Sum all the values from fromN up to toN
 }
 
